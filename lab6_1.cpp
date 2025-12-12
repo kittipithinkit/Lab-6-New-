8a65739b873fd5c013f9469006cd839a3b8c868f
ink <kittipithm@gmail.com>
2025-12-12 23:16:58 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main() {
+    int Even_Count = 0;
+    int Odd_Count = 0;
+    int Number; 
+    do {
+        cout << "Enter an integer: ";
+        cin >> Number;    
+        if ( Number != 0) {
+            if (Number % 2 == 0) {
+                Even_Count++;
+            } else {
+                Odd_Count++;
+            }
+        }
+    } while (Number != 0);
+    cout << "#Even numbers = " << Even_Count << endl;
+    cout << "#Odd numbers = " << Odd_Count << endl;
+
     return 0;
 }
